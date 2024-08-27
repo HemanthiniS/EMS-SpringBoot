d7d9d7899330e99b9e8ad187ee8ca5827e7105d6
HemanthiniS <hemajayanthi2219@gmail.com>
2024-08-27 18:34:33 +0530

    Update DepartmentController.java

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -6,6 +6,7 @@ import com.example.demo.entity.Department;
 import com.example.demo.model.DepartmentResourceModel;
 import com.example.demo.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
